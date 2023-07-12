0b0d526de667197a9f8b6691624cfe7612b85621
blinkdeploys <blinkdeploys@gmail.com>
2023-07-11 20:09:59 -0700

    toml python empty

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 # exit on error
 set -o errexit
 
-poetry lock --no-update
-poetry update
-poetry install
+# poetry lock --no-update
+# poetry update
+# poetry install
+
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
